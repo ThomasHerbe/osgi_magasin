3b76629eeb804df27b81f705d5d68fcd2f730da7
Thomas <herbelin.thomas@gmail.com>
2019-06-12 12:28:35 +0200

    Ajout du workspace

---
+++ TPLibre/Magasin/src/exception/QuantiteInsuffisanteException.java
@@ -0,0 +1,31 @@
+package exception;
+
+public class QuantiteInsuffisanteException extends Exception{
+
+	public QuantiteInsuffisanteException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public QuantiteInsuffisanteException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QuantiteInsuffisanteException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QuantiteInsuffisanteException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QuantiteInsuffisanteException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
